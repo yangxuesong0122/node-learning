116adfe378a55302b0fe61db1d4b85ec55e1730b
yxs <asong0122@163.com>
2021-10-13 21:55:21 +0800

    express初学

---
+++ code/express学习/app.js
@@ -0,0 +1,38 @@
+let express = require('express')
+
+// 创建服务器应用程序,也就是原来的 http.createServer
+let app = express()
+
+
+// 在 Express 中开放资源用一个 API 就可以公开指定目录，然后可以直接通过 /public/xx 的方式访问 public 目录中的所有资源了
+app.use('/public/', express.static('./public/'))
+app.use('/static/', express.static('./static/'))
+app.use('/node_modules/', express.static('./node_modules/'))
+
+
+app.get('/about', function (req, res) {
+  // 在 Express 中可以直接 req.query 来获取查询字符串参数
+  console.log(req.query)
+  res.send('你好，我是 Express!')
+})
+
+// 当服务器收到 get 请求 / 的时候，执行回调处理函数
+app.get('/', function (req, res) {
+  res.send(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Document</title>
+  </head>
+<body>
+  <h1>hello Express！你好</h1>
+</body>
+</html>
+`)
+})
+
+// 相当于 server.listen
+app.listen(3000, function () {
+  console.log('app is running at port 3000.')
+})
---
+++ 学习笔记/cnpm.md
@@ -0,0 +1,6 @@
+- 使用cnpm就会通过淘宝的服务器来下载
+    + npm install jquery --registry=https://registry.npm.taobao.org
+- 可以加入到配置文件中
+    + npm config set registry https://registry.npm.taobao.org
+- 查看 npm 配置信息
+    + npm config list
