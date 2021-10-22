9ec828e13dfe128b471f0b03fa8a02ca0242cc5d
yxs <asong0122@163.com>
2021-10-22 23:49:55 +0800

    登录功能

--- code/blog/app.js
+++ code/blog/app.js
@@ -18,8 +18,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(session({
+  // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+  // 目的是为了增加安全性，防止客户端恶意伪造
+  secret: 'itcast',
+  resave: false,
+  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+}))
+
 // 把路由挂载到app中
 app.use(router)
+
+// 配置一个处理 404 的中间件
+app.use(function (req, res) {
+  res.render('404.html')
+})
+
+// 配置一个全局错误处理中间件
+app.use(function (err, req, res, next) {
+  res.status(500).json({
+    err_code: 500,
+    message: err.message
+  })
+})
 app.listen(3000, () => {
     console.log('服务器已经启动.....')
 })
