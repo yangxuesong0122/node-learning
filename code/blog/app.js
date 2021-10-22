2a9ee77246a7479fe7e250302988edf6caf84560
yxs <y_xs@winning.com.cn>
2021-10-22 18:02:20 +0800

    express中的子模版喝模板继承练习

--- code/blog/app.js
+++ code/blog/app.js
@@ -6,8 +6,13 @@ let app = express()
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
 
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
+
 app.get('/', (req, res) => {
-  res.send('收到请求。。。')
+  res.render('index.html', {
+      name: '杨雪松'
+  })
 })
 app.listen(3000, () => {
     console.log('服务器已经启动.....')
